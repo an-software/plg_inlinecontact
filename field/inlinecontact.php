bdfbf615d14e0ddde2df284278d5733aeca437c6
Alexander Niklaus <contact@an-software.net>
2022-07-13 23:12:06 +0200

    Version 2.0.0

--- field/inlinecontact.php
+++ field/inlinecontact.php
@@ -3,8 +3,9 @@
  * @package     Joomla.Plugin
  * @subpackage  Content.inlinecontact
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) Alexander Niklaus. All rights reserved.
  * @license     GNU General Public License version 2 or later
+ * @link        https://www.an-software.net
  */
 
 defined('_JEXEC') or die('Restricted access');
@@ -12,22 +13,31 @@ defined('_JEXEC') or die('Restricted access');
 jimport('joomla.form.formfield');
 
 
-class JFormFieldInlineContact extends JFormField {
+class JFormFieldInlineContact extends JFormField
+{
 
-
+	/**
+	 * @var string
+	 * @since 1.0.0
+	 */
 	protected $type = 'inlinecontact';
 
-	public function getInput() {
+	/**
+	 * @return string
+	 * @since 1.0.0
+	 */
+	public function getInput()
+	{
 
 
-		$templatecount = $this->form->getValue('templatecount','params');
-		$templates = $this->form->getValue('templates','params');
+		$templatecount = $this->form->getValue('templatecount', 'params');
+		$templates     = $this->form->getValue('templates', 'params');
 
-		$output = '<div class="row-fluid">';
+		$output = '<div class="row">';
 
-		for ($i=1;$i<=$templatecount;$i++)
+		for ($i = 1; $i <= $templatecount; $i++)
 		{
-			$output .= '<div class="span3">';
+			$output .= '<div class="col-3">';
 			if (is_array($templates))
 			{
 				$exists = array_key_exists($i, $templates);
@@ -37,12 +47,12 @@ class JFormFieldInlineContact extends JFormField {
 				$exists = false;
 			}
 
-			$output .= '<strong>Template '.$i.':</strong><br>'.JText::_('PLG_INLINECONTACT_BEFORE_LABEL').'<br><textarea id="jform_params_templates'.$i.'" type="text" name="jform[params][templates]['.$i.'][b]">'.(($exists) ? $templates[$i]['b'] : '').'</textarea>';
-			$output .= '<br>'.JText::_('PLG_INLINECONTACT_TEMPLATE_LABEL').'<br><textarea id="jform_params_templates'.$i.'" type="text" name="jform[params][templates]['.$i.'][t]">'.(($exists) ? $templates[$i]['t'] : '').'</textarea>';
-			$output .= '<br>'.JText::_('PLG_INLINECONTACT_AFTER_LABEL').'<br><textarea id="jform_params_templates'.$i.'" type="text" name="jform[params][templates]['.$i.'][a]">'.(($exists) ? $templates[$i]['a'] : '').'</textarea>';
+			$output .= '<strong>Template ' . $i . ':</strong><br>' . JText::_('PLG_INLINECONTACT_BEFORE_LABEL') . '<br><textarea class="form-control" id="jform_params_templates' . $i . '" type="text" name="jform[params][templates][' . $i . '][b]">' . (($exists) ? $templates[$i]['b'] : '') . '</textarea>';
+			$output .= '<br>' . JText::_('PLG_INLINECONTACT_TEMPLATE_LABEL') . '<br><textarea class="form-control" id="jform_params_templates' . $i . '" type="text" name="jform[params][templates][' . $i . '][t]">' . (($exists) ? $templates[$i]['t'] : '') . '</textarea>';
+			$output .= '<br>' . JText::_('PLG_INLINECONTACT_AFTER_LABEL') . '<br><textarea class="form-control" id="jform_params_templates' . $i . '" type="text" name="jform[params][templates][' . $i . '][a]">' . (($exists) ? $templates[$i]['a'] : '') . '</textarea>';
 
 			$output .= '</div>';
-			if ($i%4 == 0)
+			if ($i % 4 == 0)
 			{
 				$output .= '</div><hr><div class="row-fluid">';
 			}
--- inlinecontact.php
+++ inlinecontact.php
@@ -10,37 +10,94 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
+use Joomla\String\StringHelper;
 
-class PlgContentInlineContact extends JPlugin
+class PlgContentInlineContact extends CMSPlugin implements SubscriberInterface
 {
+	/**
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var array
+	 */
 	public $params;
 
-	public function __construct(& $subject, $config)
+	/**
+	 *
+	 * @since 2.0.0
+	 *
+	 * @var bool
+	 */
+	protected $autoloadLanguage = true;
+
+
+	/**
+	 * Returns an array of events this subscriber will listen to.
+	 *
+	 * @return  array
+	 * @since 2.0.0
+	 *
+	 */
+	public static function getSubscribedEvents(): array
 	{
-		$lang =& JFactory::getLanguage();
-		$lang->load('plg_content_inlinecontact',JPATH_ADMINISTRATOR);
-		parent::__construct($subject, $config);
+		return [
+			'onContentPrepare' => 'onContentPrepare',
+		];
 	}
 
-	public function onContentPrepare($context, &$article, &$params, $page = 0)
+	/**
+	 * @return bool
+	 * @since 2.0.0
+	 *
+	 */
+	public function onContentPrepare(Event $event)
 	{
-		if (JString::strpos($article->text, '{inlinecontactlist') !== false)
+		$arguments = $event->getArguments();
+
+		return $this->handleOnContentPrepare($arguments[0], $arguments[1], $arguments[2]);
+	}
+
+
+	/**
+	 * @param     $context
+	 * @param     $article
+	 * @param     $params
+	 * @param int $page
+	 *
+	 * @return bool
+	 * @since 1.0.0
+	 *
+	 */
+	private function handleOnContentPrepare($context, &$article, &$params, $page = 0)
+	{
+		if (StringHelper::strpos($article->text, '{inlinecontactlist') !== false)
 		{
-			$regex = "/{inlinecontactlist\s(\d*?)\s(\d*?)}/";
-			$article->text = preg_replace_callback($regex, array(&$this, 'contactlist_replace_cb'), $article->text);
+			$regex         = "/{inlinecontactlist\s(\d*?)\s(\d*?)}/";
+			$article->text = preg_replace_callback($regex, array(&$this, '_contactlistReplaceCb'), $article->text);
 		}
 
-		if (JString::strpos($article->text, '{/inlinecontact}') !== false)
+		if (StringHelper::strpos($article->text, '{/inlinecontact}') !== false)
 		{
-			$regex = "/{inlinecontact\s(\d*?)}([\S\s]*?){\/inlinecontact}/";
-			$article->text = preg_replace_callback($regex, array(&$this, 'contact_replace_cb'), $article->text);
+			$regex         = "/{inlinecontact\s(\d*?)}([\S\s]*?){\/inlinecontact}/";
+			$article->text = preg_replace_callback($regex, array(&$this, '_contactReplaceCb'), $article->text);
 		}
 
 		return true;
 	}
 
 
-	protected function contact_replace_cb( &$matches ) {
+	/**
+	 * @param $matches
+	 *
+	 * @return array|string|string[]
+	 * @since 1.0.0
+	 *
+	 */
+	protected function _contactReplaceCb($matches)
+	{
 
 		$db = JFactory::getDBO();
 
@@ -48,109 +105,130 @@ class PlgContentInlineContact extends JPlugin
 		{
 			return '[invalid contact id]';
 		}
-		$contact_id = $matches[1];
-		$article_text =  $matches[2];
+		$contact_id   = intval($matches[1]);
+		$article_text = $matches[2];
 
 
 		//Get all default fields of the contact
-		$query = 'SELECT * FROM #__contact_details WHERE id =' .$contact_id;
-		$db->setQuery( $query );
+		$query = 'SELECT * FROM #__contact_details WHERE published = 1 AND id = ' . $contact_id;
+		$db->setQuery($query);
 		$contact = $db->loadObject();
 
-		if (!$contact) {
-			return '[contact not found]';
+		if (!$contact)
+		{
+			return $this->params->get('contactnotfound', JText::_('PLG_INLINECONTACT_CONTACTNOTFOUND'));
 		}
 
 		//Get all custom fields of the contact
-		$query = 'SELECT `j32_fields`.`name`, `j32_fields`.`label`, `j32_fields`.`type`, `j32_fields_values`.item_id, `j32_fields_values`.`value` FROM `j32_fields` LEFT JOIN `j32_fields_values` ON `j32_fields_values`.`field_id` = `j32_fields`.`id` AND `j32_fields_values`.`item_id` = ' .$contact_id;
-		$db->setQuery( $query );
+		$query = 'SELECT #__fields.name, #__fields.label, #__fields.type, #__fields_values.item_id, #__fields_values.value FROM #__fields LEFT JOIN #__fields_values ON #__fields_values.field_id = #__fields.id AND #__fields_values.item_id = ' . $contact_id;
+		$db->setQuery($query);
 
 		$customfields = $db->loadObjectList();
 
 
 		//Default attribute names and types
 		$attributes = array('id', 'name', 'alias', 'con_position', 'address', 'suburb', 'state', 'country', 'postcode', 'telephone', 'fax', 'misc', 'image', 'email_to', 'mobile', 'webpage');
-		$types = array('integer', 'text', 'text', 'text', 'textarea', 'text', 'text', 'text', 'text', 'text', 'text', 'editor', 'media', 'email', 'text', 'URL');
+		$types      = array('integer', 'text', 'text', 'text', 'textarea', 'text', 'text', 'text', 'text', 'text', 'text', 'editor', 'media', 'email', 'text', 'URL');
 
-		$find = array();
+		$find    = array();
 		$replace = array();
 
 		foreach ($attributes as $key => $attribute)
 		{
 			//TODO: label
-			$this->build_replace_array($find,$replace,$attribute,JText::_('PLG_INLINECONTACT_DEFLABEL_'.strtoupper($attribute)),$contact->$attribute,$types[$key]);
+			$this->buildReplaceArray($find, $replace, $attribute, JText::_('PLG_INLINECONTACT_DEFLABEL_' . strtoupper($attribute)), $contact->$attribute, $types[$key]);
 		}
 
 		foreach ($customfields as $customfield)
 		{
-			$this->build_replace_array($find,$replace,$customfield->name,$customfield->label,$customfield->value,$customfield->type);
+			$this->buildReplaceArray($find, $replace, $customfield->name, $customfield->label, $customfield->value, $customfield->type);
 		}
 
-		return str_replace($find,$replace,$article_text);
+		return str_replace($find, $replace, $article_text);
 	}
 
 
-
-	private function build_replace_array(&$find,&$replace,$name,$label,$value,$type='')
+	/**
+	 *
+	 * @param        $find
+	 * @param        $replace
+	 * @param        $name
+	 * @param        $label
+	 * @param        $value
+	 * @param string $type
+	 *
+	 * @since 1.0.0
+	 *
+	 */
+	private function buildReplaceArray(&$find, &$replace, $name, $label, $value, $type = '')
 	{
 		//Handle different field types
 		if ($type == 'media')
 		{
-			$imgPath = JURI::root() . $value;
-			if (is_file( JPATH_BASE . '/'. $value )) {
-				$value = '<img src="'. $imgPath .'" title="' . $label . '" alt="' . $label . '" />';
+			if (!empty($value))
+			{
+				$value = '<img src="' . $value . '" title="' . $label . '" alt="' . $label . '" />';
 			}
 		}
 
 		//value only
-		$find[] = "[$name]";
+		$find[]    = "[$name]";
 		$replace[] = ($this->params->get('hideempty') && $value == '') ? '' : $value;
 
 		//label only
-		$find[] = "[l_$name]";
-		$replace[] = ($this->params->get('hideempty') && $value == '') ? '' :$label;
+		$find[]    = "[l_$name]";
+		$replace[] = ($this->params->get('hideempty') && $value == '') ? '' : '<span class="plginlcon-label">' . $label . '</span>';
 
 		//label and value
-		$find[] = "[lv_$name]";
-		$replace[] = ($this->params->get('hideempty') && $value == '') ? '' : $label.': '.$value;
+		$find[]    = "[lv_$name]";
+		$replace[] = ($this->params->get('hideempty') && $value == '') ? '' : '<span class="plginlcon-label">' . $label . '</span>: ' . $value;
 	}
 
 
-	protected function contactlist_replace_cb( &$matches )
+	/**
+	 *
+	 * @param $matches
+	 *
+	 * @return string
+	 * @since 1.0.0
+	 *
+	 */
+	protected function _contactlistReplaceCb($matches)
 	{
 
-		if (!array_key_exists(1,$matches) || !is_numeric($matches[1]))
+		if (!array_key_exists(1, $matches) || !is_numeric($matches[1]))
 		{
 			return '[invalid category id]';
 		}
-		$category_id = $matches[1];
+		$category_id = intval( $matches[1] );
 
-		if (!array_key_exists(2,$matches) || !is_numeric($matches[2]))
+		if (!array_key_exists(2, $matches) || !is_numeric($matches[2]))
 		{
 			return '[invalid category id]';
 		}
 		$template_id = $matches[2];
-		$templates = $this->params->get('templates');
+		$templates   = $this->params->get('templates');
 
-		if (!is_object($templates) || !property_exists($templates,$template_id))
+		if (!is_object($templates) || !property_exists($templates, $template_id))
 		{
 			return '[invalid template id]';
 		}
 
-		$db = JFactory::getDBO();
-		$query = 'SELECT * FROM #__contact_details WHERE catid =' .$category_id;
-		$db->setQuery( $query );
+		$db    = JFactory::getDBO();
+		$query = 'SELECT * FROM #__contact_details WHERE published = 1 AND catid =' . $category_id . ' ORDER BY ordering';
+		$db->setQuery($query);
 		$contacts = $db->loadObjectList();
 
-		if (!$contacts) {
-			return '[no contact found in category]';
+		if (!$contacts)
+		{
+			return $this->params->get('nocontact', JText::_('PLG_INLINECONTACT_NOCONTACT'));
 		}
 
 		$output = $templates->$template_id->b;
 		foreach ($contacts as $contact)
 		{
-			$matches = array('',$contact->id,$templates->$template_id->t);
-			$output .= $this->contact_replace_cb( $matches );
+			$matches = array('', $contact->id, $templates->$template_id->t);
+			$output  .= $this->_contactReplaceCb($matches);
 		}
 		$output .= $templates->$template_id->a;
 
